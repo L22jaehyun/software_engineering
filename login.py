efe450cdd0b3f2d93e3bfcab7ce1ecb6e00bf982
PokingTeemo <110599863+PokingTeemo@users.noreply.github.com>
2023-05-20 01:40:29 +0900

    출금 입금 추가 및 몽고DB 연결
    0520

--- login.py
+++ login.py
@@ -1,58 +1,117 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash
+from pymongo import MongoClient
 
 app = Flask(__name__)
 app.secret_key = 'secret key'
 
-users = []
+# MongoDB 클라이언트 생성
+client = MongoClient('mongodb+srv://ys990728:ys3863228@cluster0.g6hx7ds.mongodb.net/?retryWrites=true&w=majority')
+db = client['coin_exchange']  # MongoDB 데이터베이스 선택
+users_collection = db['users']  # 사용자 컬렉션 선택
 
+
+@app.route('/', methods=['GET', 'POST']) #로그인
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        for user in users:
-            if user['username'] == username and user['password'] == password:
-                session['username'] = username
-                flash("로그인에 성공했습니다!")  # 로그인 성공 메시지 표시
-                return redirect(url_for('account'))  # 로그인 성공 시 계정 관리 페이지로 이동
-        flash("존재하지 않는 회원입니다!")
-        return render_template('login.html', register=True)
+        user = users_collection.find_one({'username': username, 'password': password})
+        if user:
+            session['username'] = username
+            flash("로그인에 성공했습니다!")
+            return redirect(url_for('account'))
+        else:
+            flash("존재하지 않는 회원입니다!")
+            return render_template('login.html', register=True)
     else:
         return render_template('login.html', register=True)
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST']) #회원가입
 def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        users.append({'username': username, 'password': password})
-        flash("회원 가입에 성공했습니다!")  # 회원 가입 성공 메시지 표시
-        return redirect(url_for('login'))
+        user = users_collection.find_one({'username': username})
+        if user:
+            flash("이미 존재하는 회원입니다!")
+            return render_template('register.html', register=True)
+        else:
+            balance = 0
+            coin = 0
+            selling_coin = 0
+            users_collection.insert_one({
+                'username': username,
+                'password': password,
+                'balance': balance,
+                'coin': coin,
+                'selling_coin': selling_coin
+            })
+            flash("회원 가입에 성공했습니다!")
+            return redirect(url_for('login'))
     else:
         return render_template('register.html', register=True)
 
-@app.route('/logout', methods=['POST'])
+@app.route('/logout') #로그아웃
 def logout():
     session.pop('username', None)
-    flash("로그아웃되었습니다!")  # 로그아웃 메시지 표시
+    flash("로그아웃되었습니다!")
     return redirect(url_for('login'))
 
-@app.route('/account')
+
+@app.route('/account') #계정
 def account():
     if 'username' in session:
-        account_number = '1234567890'  # 예시: 계좌 번호
-        balance = 1000  # 예시: 잔액
-        coins = ['Bitcoin', 'Ethereum', 'Litecoin']  # 예시: 보유 코인
-        selling_coins = ['Ripple', 'Cardano']  # 예시: 판매 중인 코인
-        return render_template('account.html', account_number=account_number, balance=balance, coins=coins, selling_coins=selling_coins)
+        user = users_collection.find_one({'username': session['username']})
+        balance = user.get('balance', 0)
+        coin = user.get('coin', 0)
+        selling_coin = user.get('selling_coin', 0)
+        return render_template('account.html', balance=balance, coin=coin,
+                               selling_coin=selling_coin)
     else:
         return redirect(url_for('login'))
 
-@app.route('/delete-account', methods=['POST'])
+
+@app.route('/delete-account', methods=['POST']) #계정탈퇴
 def delete_account():
-    # 회원 탈퇴 로직 작성
-    flash("회원 탈퇴 완료")
+    if 'username' in session:
+        users_collection.delete_one({'username': session['username']})
+        session.pop('username', None)
+        flash("회원 탈퇴 완료")
     return redirect(url_for('login'))
+
+@app.route('/deposit', methods=['POST'])  # 입금
+def deposit():
+    if 'username' in session:
+        user = users_collection.find_one({'username': session['username']})
+        balance = user.get('balance', 0)
+
+        # 사용자가 입력한 입금할 금액
+        deposit_amount = float(request.form['deposit_amount'])
+
+        updated_balance = balance + deposit_amount
+
+        users_collection.update_one({'username': session['username']}, {'$set': {'balance': updated_balance}})
+        flash("입금이 완료되었습니다!")
+    return redirect(url_for('account'))
+
+@app.route('/withdraw', methods=['POST'])  # 출금
+def withdraw():
+    if 'username' in session:
+        user = users_collection.find_one({'username': session['username']})
+        balance = user.get('balance', 0)
+
+        # 사용자가 입력한 출금할 금액
+        withdraw_amount = float(request.form['withdraw_amount'])
+
+        # 출금 가능한 잔액보다 큰 금액을 출금하려고 하면 출금되지 않음
+        if withdraw_amount > balance:
+            flash("출금 가능한 잔액보다 큰 금액을 출금할 수 없습니다!")
+        else:
+            updated_balance = balance - withdraw_amount
+            users_collection.update_one({'username': session['username']}, {'$set': {'balance': updated_balance}})
+            flash("출금이 완료되었습니다!")
+    return redirect(url_for('account'))
 
 if __name__ == '__main__':
     app.run(debug=True)
